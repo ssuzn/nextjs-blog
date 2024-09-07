1791f4c27c379c32b583dccb56bf504adde93af3
ssuzn <tnwls_723@naver.com>
2024-09-07 21:47:51 +0900

    initial

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,46 @@
+import { getAllPostIds, getPostData } from "@/lib/posts";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+import React from "react";
+
+export default function Post({
+  postData
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  }
+})  {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1></h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
+  );
+};
+
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  // [ { params: { id: 'pre-rendering' } }, { params: { id: 'ssg-ssr' } } ]
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
